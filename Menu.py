bd167cd41c032bdb05db77d8387d8c1ec36b8e96
john <jcostanzo01@manhattan.edu>
2015-12-04 12:42:51 -0500

    Clean up
    Cleaned up code. Started having problem in Menu. Number of sales doesn't
    come up and you can't add

--- Menu.py
+++ Menu.py
@@ -1,50 +1,52 @@
 from tkinter import *
 from tkinter import ttk
 
-def add(*args):
-    try:
-        value1 = int(reams.get())
-        value2 = int(moreReams.get())
-        reams.set(value1 + value2)
-    except ValueError:
-        pass
+class MenuFrame(Frame):
 
-def logout():
-    root.destroy()
+    sales = StringVar()
+
+    def __init__(self, master):
+        super().__init__(master)
+        
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
+        mainframe.columnconfigure(0, weight = 1)
+        mainframe.rowconfigure(0, weight = 1)
+
+        name = "John"
+        sales = StringVar()
+        moreSales = StringVar()
+        sales.set("1")
+
+        welcome = "Welcome, " + name + "!"
+
+        sales_entry = ttk.Entry(mainframe, width = 7, textvariable = moreSales)
+
+        ttk.Label(mainframe, text = welcome).grid(column = 2, row = 1, sticky = W)
+
+        ttk.Label(mainframe, text = "Current Sales:").grid(column = 1, row = 2, sticky = W)
+        ttk.Label(mainframe, textvariable = sales).grid(column = 2, row = 2, sticky = (E))
+        ttk.Label(mainframe, text = "sales").grid(column = 3, row = 2, sticky = W)
+
+        ttk.Label(mainframe, text = "Add Sales").grid(column = 1, row = 3, sticky = W)
+        sales_entry.grid(column = 2, row = 3, sticky = (W, E))
+        ttk.Button(mainframe, text = "Add", command = MenuFrame.add(sales, moreSales)).grid(column = 3, row = 3, sticky = W)
+
+        ttk.Button(mainframe, text = "Log Out", command = MenuFrame.logout).grid(column = 2, row = 4, sticky = W)
+
+    def add(sales, moreSales):
+        
+        try:
+            value1 = int(sales.get())
+            value2 = int(moreSales.get())
+            sales.set(value1 + value2)
+        except ValueError:
+            pass
+
+    def logout():
+        root.destroy()
 
 root = Tk()
-root.geometry('{}x{}'.format(270, 130))
 root.title("Dunder Mifflin GUI")
-
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
-mainframe.columnconfigure(0, weight = 1)
-mainframe.rowconfigure(0, weight = 1)
-
-name = "John"
-reams = StringVar()
-moreReams = StringVar()
-reams.set("1")
-
-welcome = "Welcome, " + name + "!"
-
-reams_entry = ttk.Entry(mainframe, width = 7, textvariable = moreReams)
-
-ttk.Label(mainframe, text = welcome).grid(column = 2, row = 1, sticky = W)
-
-ttk.Label(mainframe, text = "Current Sales:").grid(column = 1, row = 2, sticky = W)
-ttk.Label(mainframe, textvariable = reams).grid(column = 2, row = 2, sticky = (E))
-ttk.Label(mainframe, text = "reams").grid(column = 3, row = 2, sticky = W)
-
-ttk.Label(mainframe, text = "Add Sales").grid(column = 1, row = 3, sticky = W)
-reams_entry.grid(column = 2, row = 3, sticky = (W, E))
-ttk.Button(mainframe, text = "Add", command = add(reams, moreReams)).grid(column = 3, row = 3, sticky = W)
-
-ttk.Button(mainframe, text = "Log Out", command = logout).grid(column = 2, row = 4, sticky = W)
-
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-reams_entry.focus()
-root.bind('<Return>', add)
-
+mf = MenuFrame(root)
 root.mainloop()
